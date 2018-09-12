1cde0973aef932c21e49aaca7a806a3a48558fbf
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-12 00:25:14 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -15,5 +15,9 @@ function divSelector() {
 }
 
 function firstListItem() {
+<<<<<<< HEAD
   return $('ul li:first-child')
+=======
+  return $('u1 #pic-list')
+>>>>>>> e931cf1cce8ebc8bfa6f1d23c3810641dcc0fbda
 }
